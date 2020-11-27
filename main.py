4d9f45fc07bfdcb2dc0353363daaabc1ec4029d5
David van Egmond <davidve38@gmail.com>
2020-11-27 15:58:53 +0100

    walls around moving objects

--- main.py
+++ main.py
@@ -4,103 +4,36 @@ from easybpy import *
 import random
 from collections import defaultdict
 
-sys.path.append("C:/Users/david/OneDrive/Documenten/S4D/blender/Save_files/26_11")
+sys.path.append("C:/Users/david/OneDrive/Documenten/S4D/blender/Save_files/27_11")
 from person import *
 from material import *
+from buildings import *
 
 persons = []
-start_locations = defaultdict(list)
+walls = []
 current_locations = defaultdict(list)
-new_locations = defaultdict(list)
+next_locations = defaultdict(list)
+wall_locations = defaultdict(list)
+grid_x = 100
+grid_y = 80
 
 
 def main():
     create_collection("persons")
     set_active_collection("persons")
 
-    create_persons(20)
+    create_borders()
+    create_persons(50)
     animate_persons(20)
 
 
 def create_persons(amount):
     for i in range(amount):
-        name = "person_" + str(i)
+        name = "person_" + str(len(persons))
         person = Person(name)
         persons.append(person)
-    determine_locations_persons()
+    determine_start_locations_persons()
     draw_persons()
-
-
-def current_locations_persons():
-    i = 0
-    for pers in persons:
-        loc = tuple((pers.x, pers.y))
-        pers.current_location = loc
-        current_locations[loc].append(i)
-        i += 1
-
-
-def new_locations_persons():
-    new_locations.clear()
-    i = 0
-    for pers in persons:
-        loc = get_next_direction(pers)
-        new_locations[loc].append(i)
-        pers.x = loc[0]
-        pers.y = loc[1]
-        pers.next_location = loc
-        # print("old: " + str(pers.current_location) + " new: " + str(pers.new_location))
-        i += 1
-    walk_towards_each_other()
-
-
-def walk_towards_each_other():
-    for pers_a in persons:
-        for pers_b in persons:
-            if pers_a.current_location == pers_b.next_location and pers_b.current_location == pers_a.next_location:
-                pers_a.swap = True
-                # print(str(pers_a.name) + " and " + str(pers_b.name) + " walking towards each other")
-
-
-def get_next_direction(pers):
-    left = tuple((pers.x - pers.stepsize, pers.y))
-    up = tuple((pers.x, pers.y + pers.stepsize))
-    right = tuple((pers.x + pers.stepsize, pers.y))
-    down = tuple((pers.x, pers.y - pers.stepsize))
-    dir_array = [left, up, right, down]
-    pos_loc = []
-    for i in range(len(dir_array)):
-        if new_locations[dir_array[i]]:
-            pass
-        else:
-            pos_loc.append(dir_array[i])
-    if len(pos_loc) != 0:
-        ran_num = random.randint(0, len(pos_loc) - 1)
-        if pos_loc[ran_num] == left:
-            pers.next_direction = "left"
-        elif pos_loc[ran_num] == right:
-            pers.next_direction = "right"
-        elif pos_loc[ran_num] == down:
-            pers.next_direction = "down"
-        elif pos_loc[ran_num] == up:
-            pers.next_direction = "up"
-
-        return pos_loc[ran_num]
-
-
-def determine_locations_persons():
-    i = 0
-    while i < len(persons):
-        x = random.randint(0, 10) * 2
-        y = random.randint(0, 10) * 2
-        loc = tuple((x, y))
-        if start_locations[loc]:
-            continue
-
-        start_locations[loc].append(i)
-        persons[i].x = loc[0]
-        persons[i].y = loc[1]
-        i += 1
 
 
 def draw_persons():
@@ -111,11 +44,108 @@ def draw_persons():
 def animate_persons(steps):
     for x in range(steps):
         current_locations_persons()
-        new_locations_persons()
+        next_locations_persons()
         for person in persons:
             person.animate_step(persons)
         current_locations.clear()
-        print(str(x))
+        next_locations.clear()
+        # print(str(x))
+
+def create_borders():
+    create_wall("horizontal", grid_x / 2, grid_y)
+    create_wall("horizontal", grid_x / 2, 0)
+    create_wall("vertical", grid_x, grid_y / 2)
+    create_wall("vertical", 0, grid_y / 2)
+    wall_placement()
+    # print(wall_locations)
+
+def create_wall(rotation, x, y):
+    name = "wall_" + str(len(walls))
+    wall = Wall(name, x, y, 2)
+    wall.create(grid_x, grid_y, rotation)
+    walls.append(wall)
+
+def wall_placement():
+    i = 0
+    for wall in walls:
+        if wall.rotation == "horizontal":
+            for x_cor in range(grid_x):
+                loc = tuple((x_cor, wall.y))
+                wall_locations[loc].append(i)  # index
+                i += 1
+        else:
+            for y_cor in range(grid_y):
+                loc = tuple((wall.x, y_cor))
+                wall_locations[loc].append(i)  # index
+                i += 1
+
+def current_locations_persons():
+    i = 0
+    for pers in persons:
+        loc = tuple((pers.x, pers.y))
+        pers.current_location = loc
+        current_locations[loc].append(i)  # index
+        i += 1
+
+
+def next_locations_persons():
+    i = 0
+    for pers in persons:
+        loc = get_next_direction(pers)
+        next_locations[loc].append(i)
+        pers.x = loc[0]
+        pers.y = loc[1]
+        pers.next_location = loc
+        # print("old: " + str(pers.current_location) + " new: " + str(pers.next_location))
+        i += 1
+    check_possible_collision()
+
+
+def check_possible_collision():
+    for pers_a in persons:
+        for pers_b in persons:
+            if pers_a.current_location == pers_b.next_location and pers_b.current_location == pers_a.next_location:
+                pers_a.swap_position = True
+                # print(str(pers_a.name) + " and " + str(pers_b.name) + " walking towards each other")
+
+
+def get_next_direction(pers):
+    left = tuple((pers.x - pers.stepsize, pers.y))
+    right = tuple((pers.x + pers.stepsize, pers.y))
+    up = tuple((pers.x, pers.y + pers.stepsize))
+    down = tuple((pers.x, pers.y - pers.stepsize))
+    dir_array = [left, up, right, down]
+    pos_loc = []
+
+    for i in range(len(dir_array)):
+        if next_locations[dir_array[i]] or wall_locations[dir_array[i]]:
+            pass
+        else:
+            pos_loc.append(dir_array[i])
+
+    if len(pos_loc) != 0:
+        ran_num = get_random(0, len(pos_loc) - 1)
+
+        return pos_loc[ran_num]
+
+
+def determine_start_locations_persons():
+    i = 0
+    while i < len(persons):
+        x = get_random(0, grid_x / 2) * 2
+        y = get_random(0, grid_y / 2) * 2
+        loc = tuple((x, y))
+        if current_locations[loc]:
+            continue
+
+        current_locations[loc].append(i)
+        persons[i].x = loc[0]
+        persons[i].y = loc[1]
+        i += 1
+
+
+def get_random(a, b):
+    return random.randint(a, b)
 
 
 if __name__ == '__main__':
--- personv1.py
+++ personv1.py
@@ -3,12 +3,13 @@ import sys
 from easybpy import *
 import random
 
-sys.path.append("C:/Users/david/OneDrive/Documenten/S4D/blender/Save_files/26_11")
+sys.path.append("C:/Users/david/OneDrive/Documenten/S4D/blender/Save_files/27_11")
 from main import *
 import material
 from math import *
 
 current_locations = defaultdict(list)
+
 
 class Person(object):
     def __init__(self, name):
@@ -16,15 +17,20 @@ class Person(object):
         self.y = 0
         self.z = 0
         self.name = name
+        self.age = random.randint(0, 100)
         self.state = ""
-        self.swap = False
+        self.swap_position = False
         self.current_location = []
         self.next_location = []
         self.scale = .5
         self.frame_index = 1
         self.last_keyframe = 250
         self.stepsize = 2
-        self.animation_speed = 10
+        self.animation_speed = 20
+        self.health_score = 100
+        self.wisdom = ""
+        self.symptoms = False
+        self.infected_score = 35
 
     def create(self):
         cube = create_cube()
@@ -36,6 +42,8 @@ class Person(object):
         percentage = random.randint(1, 100)
         if percentage <= 10:
             self.state = "infected"
+            self.health_score = self.infected_score
+            self.shows_symptoms()
         else:
             self.state = "healthy"
 
@@ -45,16 +53,17 @@ class Person(object):
         self.set_scale()
         self.set_location()
         self.add_loc_keyframe()  # starting frame location
+        # print("name: " + self.name + " age: " + str(self.age) + " state: " + self.state)
 
     def create_material(self):
         new_mat = material.Material(self.name)
         new_mat.create()
         if self.state == "infected":
-            new_mat.change_color(0, 255, 0)
+            self.change_material(0)
         else:
-            new_mat.change_color(255, 255, 255)
+            self.change_material(255)
         self.add_material(self.name)
-        self.add_mat_keyframe() # starting frame material
+        self.add_mat_keyframe()  # starting frame material
 
     def add_modifier(self, name, type):
         obj = get_object(self.name)
@@ -78,37 +87,42 @@ class Person(object):
         self.frame_index += self.animation_speed  # set next frame location/time
         if self.state == "infected":
             self.check_infection_radius(persons)
+        self.regen_health_score()
         self.add_loc_keyframe()  # add keyframe for location object
         self.add_mat_keyframe()  # add keyframe for material
 
         bpy.context.scene.frame_end = self.frame_index  # last frame == end frame
 
     def add_loc_keyframe(self):
-        if self.swap == True:
-            if self.current_location[0] != self.next_location[0]:
-                if self.current_location[0] > self.next_location[0]:
-                    swap_x = self.current_location[0] - (self.stepsize / 2)
-                    swap_y = self.y + 0.5
-                else:
-                    swap_x = self.current_location[0] + (self.stepsize / 2)
-                    swap_y = self.y - 0.5
-            else:
-                if self.current_location[1] > self.next_location[1]:
-                    swap_y = self.current_location[1] - (self.stepsize / 2)
-                    swap_x = self.x + 0.5
-                else:
-                    swap_y = self.current_location[1] + (self.stepsize / 2)
-                    swap_x = self.x - 0.5
-            select_object(self.name)
-            obj = bpy.context.active_object
-            obj.location = (swap_x, swap_y, self.z)
-            obj.keyframe_insert(data_path="location", frame=(self.frame_index - (self.animation_speed / 2) ) )
-            self.swap = False
+        if self.swap_position == True:
+            self.add_swap_keyframe()
 
         select_object(self.name)
         obj = bpy.context.active_object
         obj.location = (self.x, self.y, self.z)
         obj.keyframe_insert(data_path="location", frame=self.frame_index)
+
+    def add_swap_keyframe(self):
+        if self.current_location[0] != self.next_location[0]:
+            if self.current_location[0] > self.next_location[0]:
+                swap_x = self.current_location[0] - (self.stepsize / 2)
+                swap_y = self.y + 0.5
+            else:
+                swap_x = self.current_location[0] + (self.stepsize / 2)
+                swap_y = self.y - 0.5
+        else:
+            if self.current_location[1] > self.next_location[1]:
+                swap_y = self.current_location[1] - (self.stepsize / 2)
+                swap_x = self.x + 0.5
+            else:
+                swap_y = self.current_location[1] + (self.stepsize / 2)
+                swap_x = self.x - 0.5
+
+        select_object(self.name)
+        obj = bpy.context.active_object
+        obj.location = (swap_x, swap_y, self.z)
+        obj.keyframe_insert(data_path="location", frame=(self.frame_index - (self.animation_speed / 2)))
+        self.swap_position = False
 
     def add_mat_keyframe(self):
         ob = bpy.context.object
@@ -120,21 +134,19 @@ class Person(object):
             mat.keyframe_insert(data_path="diffuse_color", frame=self.frame_index)
 
     def add_material(self, material):
-        D = bpy.data
-
-        if len(D.objects[self.name].material_slots) < 1:
+        if len(bpy.data.objects[self.name].material_slots) < 1:
             # if there is no slot then we append to create the slot and assign
-            D.objects[self.name].data.materials.append(D.materials[material])
+            bpy.data.objects[self.name].data.materials.append(bpy.data.materials[material])
         else:
             # we always want the material in slot[0]
-            D.objects[self.name].material_slots[0].material = D.materials[material]
+            bpy.data.objects[self.name].material_slots[0].material = bpy.data.materials[material]
 
-    def change_material(self, r, g, b):
-        select_object(self.name)
-        obj = bpy.context.active_object
-        obj.active_material.diffuse_color[0] = r / 255
-        obj.active_material.diffuse_color[1] = g / 255
-        obj.active_material.diffuse_color[2] = b / 255
+    def change_material(self, intensity_color):
+        mat = material.Material(self.name)
+        if self.symptoms == True:
+            mat.change_color(intensity_color, 255, intensity_color)
+        else:
+            mat.change_color(intensity_color, intensity_color, 255)
 
     def check_infection_radius(self, persons):
         social_distance = 1.5 + self.scale
@@ -146,8 +158,24 @@ class Person(object):
 
     def infect_other(self, pers):
         percentage = random.randint(1, 100)
-        if percentage <= 20:
+        if percentage <= 100:
             pers.state = "infected"
-            pers.change_material(0, 255, 0)
+            pers.health_score = pers.infected_score
+            self.shows_symptoms()
+            pers.change_material(0)
 
+    def shows_symptoms(self):
+        ran_num = random.randint(0, self.age)
+        if ran_num >= 18 or self.age >= 70:
+            self.symptoms = True
 
+    def regen_health_score(self):
+        if self.health_score != 100:
+            self.health_score += 5
+
+            if self.health_score >= 100:
+                self.health_score = 100
+                self.state = "healthy"
+
+        intensity = (self.health_score - self.infected_score) * (255 / (100 - self.infected_score))
+        self.change_material(intensity)
